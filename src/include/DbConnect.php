b1e5ce54293a81ef7568a60c8074a62072316d32
David Courtney <dsbcourtney@gmail.com>
2015-02-25 20:27:06 +0000

    Old changes

--- src/include/DbConnect.php
+++ src/include/DbConnect.php
@@ -1,35 +1,34 @@
 <?php
- 
 /**
  * Handling database connection
  *
- * @author Ravi Tamada
+ * @author David Courtney
  */
+
 class DbConnect {
  
-    private $conn;
- 
-    function __construct() {        
+  private $conn;
+
+  function __construct() {        
+  }
+
+  /**
+   * Establishing database connection
+   * @return database connection handler
+   */
+  function connect() {
+    include_once dirname(__FILE__) . './Config.php';
+
+    // Connecting to mysql database
+    $this->conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+    // Check for database connection error
+    if (mysqli_connect_errno()) {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
- 
-    /**
-     * Establishing database connection
-     * @return database connection handler
-     */
-    function connect() {
-        include_once dirname(__FILE__) . './Config.php';
- 
-        // Connecting to mysql database
-        $this->conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
-        // Check for database connection error
-        if (mysqli_connect_errno()) {
-            echo "Failed to connect to MySQL: " . mysqli_connect_error();
-        }
- 
-        // returing connection resource
-        return $this->conn;
-    }
+
+    // returing connection resource
+    return $this->conn;
+  }
  
 }
- 
